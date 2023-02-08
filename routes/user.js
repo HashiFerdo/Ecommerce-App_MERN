3a441696c53b596b42d1038013ccb1ee05b9e3ad
Hashiprabha Fernando <HashiprabhaF@nsmnf.local>
2023-02-08 11:15:36 +0530

    New

---
+++ routes/user.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const router = express.Router();
+
+const {
+    requireSignin
+}= require("../controllers/auth");
+
+const {userById} = require("../controllers/user");
+
+router.get('/secret/:userId',requireSignin,(req,res) =>{
+    res.json({
+      user: req.profile
+    })
+});
+
+
+router.param('userId',userById);
+
+
+
+// router.get("/hello",requireSignin,(req,res)=>{
+//   res.send("hello");
+// });
+
+
+  module.exports = router;
