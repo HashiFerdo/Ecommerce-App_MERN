9accac0bbae39f4341aa9e9b7ca831dc3cae3647
HashiFerdo <hashifernando1999@gmail.com>
2023-02-10 12:40:58 +0530

    category

--- routes/category.js
+++ routes/category.js
@@ -2,14 +2,24 @@ const express = require('express');
 const router = express.Router();
 
 
-const {
-    create
-} = require("../controllers/category");
+const {create} = require("../controllers/category");
+const { requireSignin, isAuth, isAdmin } = require("../controllers/auth");
+const { userById } = require("../controllers/user");
 
-const {requireSignin, isAuth, isAdmin } = require("../controllers/auth")
+// const {requireSignin, isAuth, isAdmin } = require("../controllers/auth")
+
+// const { userById } = require("../controllers/user")
 
 
-router.post("/category/create", create);
+
+router.post("/category/create/:userId",
+requireSignin,
+isAuth,
+isAdmin,
+create
+);
+
+router.param("userId",userById);
 
 
 // router.get("/hello",requireSignin,(req,res)=>{
